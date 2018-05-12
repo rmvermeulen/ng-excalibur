50aa04c9816e7bab0a275886550a73241fc05308
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2018-05-12 16:12:36 +0200

    more specific glob for formatter

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // consistent package.json formatting
   'package.json': ['yarn format-package -w', 'git add'],
   // format any file in src
-  'src/**/*': ['yarn format', 'git add'],
+  'src/**/*.{ts,json}': ['yarn format', 'git add'],
   // lint-fix and test and typescript file
   'src/**/*.ts': ['yarn fix:show-errors', 'git add', 'jest --findRelatedTests'],
 }
